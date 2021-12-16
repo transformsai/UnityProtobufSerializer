aff42be31bad955b8ca7cebe77cfab1b1c997118
Dante C <hello@mhdante.com>
2021-12-16 17:45:15 -0500

    Update LinkXMLInstaller to prevent warnings

--- Editor/LinkXmlInstaller.cs
+++ Editor/LinkXmlInstaller.cs
@@ -8,17 +8,20 @@ using UnityEngine;
 
 namespace TransformsAI.Unity.Protobuf.Editor
 {
-    internal class LinkXmlInstaller  : ScriptableObject, IUnityLinkerProcessor
+    internal class LinkXmlInstaller : ScriptableObject
     {
+        // Set the static reference on the script asset to link.xml
         public TextAsset XmlFile;
+    }
 
+    internal class LinkXmlInstallerEditor : IUnityLinkerProcessor
+    {
         int IOrderedCallback.callbackOrder => 0;
         string IUnityLinkerProcessor.GenerateAdditionalLinkXmlFile(BuildReport report, UnityLinkerBuildPipelineData data)
         {
-
             // Uses Static reference on the script to get link.xml
-            var obj = CreateInstance<LinkXmlInstaller>();
-            var assetPath = AssetDatabase.GetAssetPath(XmlFile);
+            var obj = ScriptableObject.CreateInstance<LinkXmlInstaller>();
+            var assetPath = AssetDatabase.GetAssetPath(obj.XmlFile);
             // assets paths are relative to the unity project root, but they don't correspond to actual folders for
             // Packages that are embedded. I.e. it won't work if a package is installed as a git submodule
             // So resolve it to an absolute path:
